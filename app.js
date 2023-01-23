687754ac21842518e5676fae982ae3e9157c85df
lgdoliveira <lgdoliveira@rd.com.br>
2023-01-23 13:41:11 -0300

    Commit pra teste api na aws

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require("cors");
+const app = express()
+
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+ }
+ 
+app.use(cors(corsOptions))
+app.use(express.json())
+
+app.listen(5001, () => console.log('api running on port 5001'))
+
+app.get('/', (res) => {
+    res.status(200).json({msg: 'Bem vindo a nossa API'})
+})
+
