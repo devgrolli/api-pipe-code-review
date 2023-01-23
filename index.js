e343c53aab44848233debbf62b6cbbb0892eaf55
lgdoliveira <lgdoliveira@rd.com.br>
2023-01-23 14:19:10 -0300

    Teste api na aws

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-// require('dotenv').config()
+require('dotenv').config()
 const express = require('express')
-// const cors = require("cors");
+const cors = require("cors");
 const app = express()
+
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+ }
+ 
+app.use(cors(corsOptions))
 app.use(express.json())
 
-// const corsOptions = {
-//     origin: '*',
-//     credentials: true,
-//     optionSuccessStatus: 200,
-//  }
- 
-// app.use(cors(corsOptions))
-
-app.get('/', (resonse) => {
-    return resonse.json({message: "Server is up"});
-})
-
-app.post('/', (request, resonse) => {
-    const { name, date } = request.body;
-    return resonse.json({name, date});
+app.get('/', (res) => {
+    res.status(200).json({msg: 'Bem vindo a nossa API'})
 })
 
 app.listen(3333, console.log('listening on port 3333'))
